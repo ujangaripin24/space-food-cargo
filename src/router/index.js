7b4dad8eff2e8d29291e2c12cdba28ca9d62fd4a
Aripin2401 <ujangarifin06@gmail.com>
2022-10-15 14:34:34 +0700

    3. API Login Form telah terinstall

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +15,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardView
   }
 ]
 
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- simple sidebar -->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+        <v-list dense>
+            <v-list-item link>
+            <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+            <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+                <v-list-item-title>Profile</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+            <v-list-item-icon>
+                <v-icon>mdi-settings</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+                <v-list-item-title>Settings</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    <!-- simple sidebar -->
+</template>
