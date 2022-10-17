98e777c759906e05b1975205ae92885cd4c33b4b
Aripin2401 <ujangarifin06@gmail.com>
2022-10-17 19:45:39 +0700

    7. Dashboard Makanan telah terinstall

--- src/views/User/DashboardView.vue
+++ src/views/User/DashboardView.vue
@@ -7,7 +7,7 @@
     >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Menu Makan Receh</v-toolbar-title>
     </v-app-bar>
 
     <v-navigation-drawer
@@ -59,10 +59,31 @@
       </div>
     </template>
     </v-navigation-drawer>
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
+      <div class="ml-2">
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="food in foods"
+              :key="food"
+            >
+              <v-card height="300" width="400">
+                <v-img
+                  :src="food.img"
+                  height="170"
+                  width="400"
+                ></v-img>
+                <v-card-title>
+                  {{ food.name }}
+                </v-card-title>
+                <v-card-subtitle>
+                  <h5>Rp. {{ food.price }}</h5>
+                </v-card-subtitle>
+                  <v-btn class="ml-2" color="primary" @click="addToCart(food)"><v-icon>mdi-cart</v-icon> Add to Cart</v-btn>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
   </v-app>
 </template>
 
@@ -76,7 +97,12 @@
         { id: 2, title: 'Users', icon: 'mdi-account-multiple', to: '/user/users' },
         { id: 3, title: 'Settings', icon: 'mdi-cog', to: '/user/setting' },
       ],
-      
+      foods: [
+        // random food name, price, img,
+        { id: 1, name: 'Nasi Padang', price: 10000, img: 'https://assets-pergikuliner.com/uploads/bootsy/image/18948/Nasi_Padang__pergikuliner.com_.jpeg' },
+        { id: 2, name: 'Nasi Uduk', price: 15000, img: 'https://assets.promediateknologi.com/crop/0x0:0x0/750x500/photo/2022/08/03/765351270.png' },
+        { id: 3, name: 'Nasi Goreng', price: 14000, img: 'https://assets.pikiran-rakyat.com/crop/75x53:675x478/750x500/photo/2022/06/06/2861416974.jpg' },
+        ]
     }),
   }
 </script>
